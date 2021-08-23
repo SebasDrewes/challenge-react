53dc784d3aa7f53b00707c067dcd45e2e9707b81
sebas <sebas.drewes@gmail.com>
2021-08-23 20:05:45 -0300

    testeando post

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
 import './App.css';
+//import { useField } from 'formik';
+
 
 function App() {
+  const [data, setData] = useState([])
+
+  const validateLogin = async () => {
+    const credentials = {
+      email: 'challenge@alkemy.org',
+      password: 'react'
+    }
+    const response = await axios.post('http://challenge-react.alkemy.org/', credentials)
+    setData(response)
+  }
+  useEffect (() => {
+    validateLogin();
+  }, [])
   return (
     <div className="App">
+      <button onClick={() => console.log(data)}>console</button>
     </div>
   );
 }
