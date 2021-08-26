24d9bab10fbe964d8952723eb42e255b34f56289
sebas <sebas.drewes@gmail.com>
2021-08-25 21:54:22 -0300

    agregado display fetched heroes

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,20 +1,40 @@
+import { useState } from 'react'
 import { Redirect} from 'react-router-dom'
 import { Formik, Field, Form } from "formik";
 import axios from 'axios';
-
+import './Search.css'
 const Search = ({authorized}) => {
+    const [heroes, setHeroes] = useState([])
 
-if(!authorized){
-    return <Redirect to="/login" />
-  }
-const searchHero = async (value) => {
+    if(!authorized){
+    return <Redirect to="/login" /> }
+
+  const searchHero = async (value) => {
+    if (value){
+    const results = await axios.get(`https://www.superheroapi.com/api.php/4333347540058740/search/${value}`)
+    setHeroes(results.data.results)
+}
+}
+const displaySearchHeroes = () => {
+    console.log(heroes)
+    const fetchedHeroes = [];
+    for (let i = 0; i < heroes.length; i += 1) {
+        fetchedHeroes.push(
+            <div key={heroes[i].id} className="flexColumn">
+                <h1>{heroes[i].name}</h1>
+                <img src={heroes[i].image.url} alt={heroes[i].name}/>
+                <button>Add Hero</button>
+            </div>
+        )
+    }
+    return fetchedHeroes || null
 
 }
     return (
-        <div id="form">
+        <div>
           <Formik
             initialValues={{ search: ""}}
-            onSubmit={(value) => {searchHero(value)}}
+            onSubmit={(value) => {searchHero(value.search)}}
           >
               <Form>
             <div className="mb-3">
@@ -24,6 +44,9 @@ const searchHero = async (value) => {
               <button type="submit" className="btn btn-dark">Ingresar</button>
             </Form>
           </Formik>
+          <div id="flex">
+          {displaySearchHeroes()}
+          </div>
         </div>
   );
 }
