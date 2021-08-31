148aacb62cf0ac4dd265317a75c7a9360e4ab8fc
sebas <sebas.drewes@gmail.com>
2021-08-30 22:30:33 -0300

    agregado progress bar team

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -22,12 +22,30 @@ const Team = ({team, setTeam}) => {
              Powerstats
             </button>
               <ul className="dropdown-menu">
+                <div class="progress-bar" role="progressbar" style={{width:`${hero.powerstats.combat}%`}}
+                aria-valuenow={hero.powerstats.combat} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Combate: </strong>{hero.powerstats.combat}</li>
+                </div>
+                <div class="progress-bar text-white" role="progressbar" style={{width:`${hero.powerstats.durability}%`}}
+                aria-valuenow={hero.powerstats.durability} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Durabilidad: </strong>{hero.powerstats.durability}</li>
+                </div>
+                <div class="progress-bar text-white" role="progressbar" style={{width:`${hero.powerstats.intelligence}%`}}
+                aria-valuenow={hero.powerstats.intelligence} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Inteligencia: </strong>{hero.powerstats.intelligence}</li>
+                </div>
+                <div class="progress-bar text-white" role="progressbar" style={{width:`${hero.powerstats.power}%`}}
+                aria-valuenow={hero.powerstats.power} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Poder: </strong>{hero.powerstats.power}</li>
+                </div>
+                <div class="progress-bar text-white" role="progressbar" style={{width:`${hero.powerstats.speed}%`}}
+                aria-valuenow={hero.powerstats.speed} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Velocidad: </strong>{hero.powerstats.speed}</li>
+                </div>
+                <div class="progress-bar text-white" role="progressbar" style={{width:`${hero.powerstats.strength}%`}}
+                aria-valuenow={hero.powerstats.strength} aria-valuemin="0" aria-valuemax="100">
                 <li className="powerstat"><strong>Fuerza: </strong>{hero.powerstats.strength}</li>
+                </div>
               </ul>
             <Link to={`/${hero.id}`}><button className="btn btn-dark btnTeam" >Detalles</button></Link>
             <button className="btn btn-dark btnTeam" onClick={() => deleteHero(hero.id)}>Eliminar</button>
