0b0ae665e789fd73e1beece0236f73426d3da550
sebas <sebas.drewes@gmail.com>
2021-08-31 00:15:02 -0300

    empezada logica stats

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -1,20 +1,30 @@
 const Stats = ({team}) => {
     console.log(team)
-    let totalCombate = 0
-    team.forEach(hero => totalCombate += parseInt(hero.powerstats.combat));
-    console.log(totalCombate)
+    let [totalCombate, totalDurabilidad, totalInteligencia,
+    totalPoder, totalVelocidad, totalFuerza]  = Array(6).fill(0)
+
+    team.forEach(hero => {
+        console.log(totalCombate)
+        totalCombate += parseInt(hero.powerstats.combat);
+        totalDurabilidad += parseInt(hero.powerstats.durability);
+        totalInteligencia += parseInt(hero.powerstats.intelligence);
+        totalPoder += parseInt(hero.powerstats.power);
+        totalVelocidad += parseInt(hero.powerstats.speed);
+        totalFuerza += parseInt(hero.powerstats.strength);
+    })
+
     return (
         <div>
             <h1><u>Estadisticas</u></h1>
             <p><strong>Tipo de Equipo: </strong>{'asd'} </p>
             <p><strong>Peso promedio: </strong>{'hero.powerstats.speed'}</p>
             <p><strong>Altura promedio: </strong>{'hero.powerstats.strength'}</p>
-            <p><strong>Combate: </strong>{'hero.powerstats.combat'}</p>
-            <p><strong>Durabilidad: </strong>{'hero.powerstats.durability'}</p>
-            <p><strong>Inteligencia: </strong>{'hero.powerstats.intelligence'}</p>
-            <p><strong>Poder: </strong>{'hero.powerstats.power'}</p>
-            <p><strong>Velocidad: </strong>{'hero.powerstats.speed'}</p>
-            <p><strong>Fuerza: </strong>{'hero.powerstats.strength'}</p>
+            <p><strong>Combate: </strong>{totalCombate}</p>
+            <p><strong>Durabilidad: </strong>{totalDurabilidad}</p>
+            <p><strong>Inteligencia: </strong>{totalInteligencia}</p>
+            <p><strong>Poder: </strong>{totalPoder}</p>
+            <p><strong>Velocidad: </strong>{totalVelocidad}</p>
+            <p><strong>Fuerza: </strong>{totalFuerza}</p>
         </div>
     )
 }
